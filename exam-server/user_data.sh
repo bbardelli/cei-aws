c4778c947e412ee8ed44e4378152e29353fce1ff
Bruno Augusto Bardelli <bbardelli@duosecurity.com>
2024-03-20 21:46:29 +0100

    feat:added user data

---
+++ exam-server/user_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+sudo yum install git -y
+sudo pip3 install cherrypy
+sudo pip3 install boto3
+git clone https://github.com/bbardelli/cei-aws.git
+export DYNAMO_AWS_REGION=eu-south-2
+export DYNAMO_TABLE=my_exam_table
+sudo python3 cei-aws/exam-server/app.py
