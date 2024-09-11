c3079749061a449bd0e369dcad2642645b368c1d
Bruno Augusto Bardelli <bbardelli@duosecurity.com>
2024-09-11 21:38:17 +0200

    fix: add redis

--- exam-server/user_data.sh
+++ exam-server/user_data.sh
@@ -5,6 +5,7 @@ sudo yum install python3-pip -y
 sudo yum install git -y
 sudo pip3 install cherrypy
 sudo pip3 install boto3
+sudo pip3 install redis
 git clone https://github.com/bbardelli/cei-aws.git
 export DYNAMO_AWS_REGION=eu-south-2
 export DYNAMO_TABLE=my_exam_table
