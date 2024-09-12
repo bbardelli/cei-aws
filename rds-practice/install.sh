dd0aebfc2827bbc6d1507606abb27b1fa631bee8
Bruno Augusto Bardelli <bbardelli@duosecurity.com>
2024-09-12 21:24:00 +0200

    fix for ubuntu

--- rds-practice/install.sh
+++ rds-practice/install.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get install python3-pip -y
+sudo apt install python3-pymysql -y
 sudo apt-get install mysql-client -y
 git clone https://github.com/bbardelli/cei-aws.git
-export RDS_HOST=mydbinstance.cjxjxjxjxjxj.eu-west-1.rds.amazonaws.com
+export RDS_HOST=database-1.cre4usso2lld.eu-south-2.rds.amazonaws.com
 mysql -h $RDS_HOST -u admin -p < cei-aws/rds-practice/script.sql
 mysql -h $RDS_HOST -u admin -D dummy -p < cei-aws/rds-practice/dummy-data/dummy.sql
