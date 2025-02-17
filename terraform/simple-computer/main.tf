7bce771e9b160c26da2e022806a4705b2484ef9a
Bruno Augusto Bardelli <bbardelli@duosecurity.com>
2025-02-17 20:13:27 +0100

    better spot

--- terraform/simple-computer/main.tf
+++ terraform/simple-computer/main.tf
@@ -1,13 +1,18 @@
+locals {
+  use_spot = false
+}
+
+
 provider "aws" {
-  region = "eu-south-2"
+  region = "us-east-1"
 }
-terraform {
-  backend "s3" {
-    bucket = "bucket-cei-test"
-    key    = "simple-computer/terraform.tfstate"
-    region = "eu-south-2"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "bucket-cei-test"
+#     key    = "simple-computer/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 #get latest ami id for amazon linux 2023
 data "aws_ami" "amazon_linux_2023" {
   most_recent = true
@@ -27,6 +32,7 @@ data "aws_ami" "amazon_linux_2023" {
 
 #create a simple ec2 instance with user data
 resource "aws_spot_instance_request" "simple-computer" {
+  count                       = local.use_spot ? 1 : 0
   ami                         = data.aws_ami.amazon_linux_2023.id
   instance_type               = "t3.micro"
   spot_type                   = "one-time"
@@ -56,6 +62,40 @@ resource "aws_spot_instance_request" "simple-computer" {
   security_groups             = [aws_security_group.ingress-test.id]
   subnet_id                   = data.aws_subnets.default.ids[0]
   wait_for_fulfillment        = true
+  tags = {
+    name = "simple-computer"
+  }
+}
+
+resource "aws_instance" "simple-computer" {
+  count                       = local.use_spot ? 0 : 1
+  ami                         = data.aws_ami.amazon_linux_2023.id
+  instance_type               = "t3.small"
+  associate_public_ip_address = true
+  user_data                   = <<-EOFILE
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install python3 -y
+                sudo yum install python3-pip -y
+                sudo pip3 install cherrypy
+                # Create a Python file
+                cat > hello_world.py <<EOF
+                import cherrypy
+
+                class HelloWorld:
+                    @cherrypy.expose
+                    def index(self):
+                        return "Hola Mundo!"
+
+                if __name__ == '__main__':
+                    cherrypy.config.update({'server.socket_port': 80, 'server.socket_host': '0.0.0.0',})
+                    cherrypy.quickstart(HelloWorld())
+                EOF
+                # Run the server (may need sudo to bind to port 80)
+                sudo python3 hello_world.py
+              EOFILE
+  security_groups             = [aws_security_group.ingress-test.id]
+  subnet_id                   = data.aws_subnets.default.ids[0]
   tags = {
     name = "simple-computer"
   }
--- terraform/simple-computer/output.tf
+++ terraform/simple-computer/output.tf
@@ -1,6 +1,6 @@
 #get spot instance request public ip
 output "public_ip" {
-  value = aws_spot_instance_request.simple-computer.public_ip
+  value = local.use_spot ? aws_spot_instance_request.simple-computer.0.public_ip : aws_instance.simple-computer.0.public_ip
 }
 output "azs_available" {
   value = local.azs
